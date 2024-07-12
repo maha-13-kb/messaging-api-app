4a2ae9351296061355ea294ff8b1a5327875591c
maha-13-kb <mahashruthispips@gmail.com>
2024-07-12 15:11:46 +0530

    changes the spring function

--- src/main/java/com/training/oracle/bms/MessagingApiAppApplication.java
+++ src/main/java/com/training/oracle/bms/MessagingApiAppApplication.java
@@ -3,13 +3,15 @@ package com.training.oracle.bms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MessagingApiAppApplication {
-
-	public void printGreetings() {
-		System.out.println("Hi Oracle");
-		
+	
+	@GetMapping(name = "enjoy")
+	public String printGreetings() {
+		return "Hi Oracle";
 	}
 	
 	public static void main(String[] args) {
